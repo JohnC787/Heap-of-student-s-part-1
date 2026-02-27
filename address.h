9e2c8ca3a2a53e3bb35ddc69cf78e64bfb6c0264
JohnC787 <johncolligan2005@yahoo.com>
2026-02-27 03:35:16 -0500

    Create address.h

---
+++ address.h
@@ -0,0 +1,23 @@
+#ifndef ADDRESS_H
+#define ADDRESS_H
+
+#include <string>
+
+class Address {
+private:
+    std::string street;
+    std::string city;
+    std::string state;
+    std::string zip;
+
+public:
+    Address();
+    void init(std::string street,
+              std::string city,
+              std::string state,
+              std::string zip);
+
+    void printAddress() const;
+};
+
+#endif
