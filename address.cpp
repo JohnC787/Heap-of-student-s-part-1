181bb4fce5168b647ea37bab457e228ecad95480
JohnC787 <johncolligan2005@yahoo.com>
2026-02-27 03:34:42 -0500

    Create address.cpp

---
+++ address.cpp
@@ -0,0 +1,26 @@
+#include "address.h"
+#include <iostream>
+
+Address::Address() {
+    street = "";
+    city = "";
+    state = "";
+    zip = "";
+}
+
+void Address::init(std::string s,
+                   std::string c,
+                   std::string st,
+                   std::string z) {
+    street = s;
+    city = c;
+    state = st;
+    zip = z;
+}
+
+void Address::printAddress() const {
+    std::cout << street << std::endl;
+    std::cout << city << " "
+              << state << ", "
+              << zip << std::endl;
+}
